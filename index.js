5176e61c8ef75b5cbec80e9acadac8b1db65a270
mubin <mubinulislam14@gmail.com>
2024-12-22 22:09:57 +0600

    add functionality for search any service by id in all services

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
@@ -29,7 +29,12 @@ async function run() {
         const result = await cursor.toArray()
         res.send(result)
       })
-
+      app.get("/service/:id", async (req, res) => {
+        let id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await serviceCollection.findOne(query);
+        res.send(result);
+      });
       app.post('/service', async(req, res)=>{
         let service = req.body
         console.log(service)
